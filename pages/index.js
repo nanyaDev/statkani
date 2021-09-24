9010d799f95aeb5ae1bec4f21aeaebb1cda72e1b
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-09-24 15:00:16 +0530

    Start landing page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 const Home = () => {
-  return <h1 className="font-bold">Hello from Homepage!</h1>;
+  return (
+    <div className="flex-grow flex flex-col justify-center items-center">
+      <h1 className="text-7xl text-center font-black text-gray-700 mb-16">
+        API Key お願いします
+      </h1>
+      <input
+        className="w-full max-w-lg border-b-2 text-xl font-light text-center focus:outline-none focus:placeholder-transparent py-2"
+        placeholder="Your key goes here!"
+      />
+    </div>
+  );
 };
 
 export default Home;
