9bcacb881ea18d4b77efa89aa1607610fd36d11e
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-09-25 17:48:07 +0530

    Implement navbar

---
+++ pages/graphs.js
@@ -0,0 +1,11 @@
+import Navbar from '@/components/Navbar';
+
+const Graphs = () => {
+  return (
+    <div className="flex-grow flex flex-col bg-bg">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Graphs;
