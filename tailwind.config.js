a5c953aef233b7e46d16cc43c79f1b5eea9958ec
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-09-25 09:50:09 +0530

    Customize tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: '#FF00AA',
+        blue: '#00AAFF',
+        purple: '#A100F1',
+        bg: '#050B20',
+        fg: '#11162D',
+        gray: {
+          1: '#F4F4F4',
+          2: '#E0E0E0',
+          3: '#555555',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
