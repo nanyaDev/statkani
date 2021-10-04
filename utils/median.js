249b141fdd8e84cb6a7d289ebe9d22e03fe60354
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-10-04 13:22:29 +0530

    Abstract helper functions

---
+++ utils/median.js
@@ -0,0 +1,12 @@
+const median = (arr) => {
+  const sorted = arr.slice().sort((a, b) => a - b);
+  const middle = Math.floor(sorted.length / 2);
+
+  if (sorted.length % 2 === 0) {
+    return (sorted[middle - 1] + sorted[middle]) / 2;
+  }
+
+  return sorted[middle];
+};
+
+export default median;
