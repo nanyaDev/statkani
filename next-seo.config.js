ba75cf2a3f20d25cebccd163a77489916921e909
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-10-08 18:58:53 +0530

    Implement barebones SEO

---
+++ next-seo.config.js
@@ -0,0 +1,8 @@
+const SEO = {
+  titleTemplate: 'StatKani | %s',
+  defaultTitle: 'StatKani',
+  description: 'StatKani is a data visualization site for users of WaniKani.',
+  canonical: 'https://statkani.vercel.app',
+};
+
+export default SEO;
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
+import { DefaultSeo } from 'next-seo';
+import { useRouter } from 'next/router';
+
 import '@/styles/globals.css';
 import { AuthProvider } from '@/lib/auth';
 import { ConditionalDataProvider } from '@/lib/data';
+import SEO from '../next-seo.config';
 
 function MyApp({ Component, pageProps }) {
+  const { pathname } = useRouter();
+
+  const path = pathname.split('/')[1];
+  const title = path.charAt(0).toUpperCase() + path.slice(1);
+
   return (
     <AuthProvider>
       <ConditionalDataProvider exclude={['/']}>
+        <DefaultSeo {...SEO} title={title} />
         <Component {...pageProps} />
       </ConditionalDataProvider>
     </AuthProvider>
