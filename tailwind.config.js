d172723c5edd5a7832ba7bdc5abfd014228debb2
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-10-19 16:53:07 +0530

    Implemenet desktop-first breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,12 @@ module.exports = {
         },
       },
     },
+    screens: {
+      xl: { max: '1280px' },
+      lg: { max: '1024px' },
+      md: { max: '768px' },
+      sm: { max: '640px' },
+    },
   },
   variants: {
     extend: {},
