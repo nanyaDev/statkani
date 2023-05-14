1a1147d45210213ddc953537f0219a8b5c8ea5eb
Rishabh Chatterjee <40901142+nanyaDev@users.noreply.github.com>
2023-05-14 11:45:45 +0100

    Update styles for loader

--- components/Loader.js
+++ components/Loader.js
@@ -11,19 +11,28 @@ const Loader = ({ progress }) => {
   );
 };
 
+// cf. https://tailwind-elements.com/docs/standard/components/progress/
 const ProgressBar = ({ endpoint, progress }) => {
   return (
-    <div className="relative max-w-sm w-full h-10 px-6 flex justify-between items-center rounded-full bg-gray-3 overflow-hidden">
-      <div
-        className="absolute left-0 top-0 bottom-0 bg-blue"
-        style={{ width: `${progress * 100}%` }}
-      />
-      <span className="text-gray-1 font-medium z-10">
-        {capitalize(endpoint)}
-      </span>
-      <span className="text-gray-1 font-medium z-10">
-        {toPercentage(progress)}
-      </span>
+    <div className="max-w-sm w-full relative">
+      <div className="flex items-center justify-between">
+        <div>
+          <span className="text-xs font-semibold inline-block pb-2 px-2 uppercase rounded-full text-white">
+            {capitalize(endpoint)}
+          </span>
+        </div>
+        <div className="text-right">
+          <span className="text-xs font-semibold inline-block pb-2 px-2 text-white">
+            {toPercentage(progress)}
+          </span>
+        </div>
+      </div>
+      <div className="overflow-hidden h-1 text-xs flex rounded bg-blue-200">
+        <div
+          style={{ width: `${progress * 100}%` }}
+          className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue"
+        ></div>
+      </div>
     </div>
   );
 };
