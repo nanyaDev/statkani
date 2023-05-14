fcf925b9832f45b0ddebe7c1f421ee29ce0aa745
Rishabh Chatterjee <40901142+nanyaDev@users.noreply.github.com>
2023-05-14 11:45:45 +0100

    Setup redirect if unauthenticated

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'cookie',
+            key: 'token',
+          },
+        ],
+        destination: '/dashboard',
+        permanent: false,
+      },
+    ];
+  },
 };
