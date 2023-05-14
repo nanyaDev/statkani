274616a1530e5f57b2416fa8853818db30c9da38
Rishabh Chatterjee <40901142+nanyaDev@users.noreply.github.com>
2023-05-14 11:45:45 +0100

    Add tooltips to graphs page

--- utils/getDuration.js
+++ utils/getDuration.js
@@ -1,11 +1,11 @@
 import { intervalToDuration, formatDuration } from 'date-fns';
 
-const getDuration = (s, e, format) => {
+const getDuration = (s, e, format, zero = false) => {
   const start = new Date(s);
   const end = e === 'now' ? new Date() : new Date(e); // ? is there a better way that 'now'
 
   const duration = intervalToDuration({ start, end });
-  const formatted = formatDuration(duration, { format });
+  const formatted = formatDuration(duration, { format, zero });
 
   return formatted;
 };
