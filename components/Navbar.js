5d202d5b9901f8aaba8ad27149333d2b11cb4a9e
Rishabh Chatterjee <40901142+nanyaDev@users.noreply.github.com>
2023-05-14 11:45:44 +0100

    Implement navbar

---
+++ components/Navbar.js
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { AiFillDashboard, AiOutlineBars } from 'react-icons/ai';
+import { MdShowChart } from 'react-icons/md';
+import { FaBookOpen, FaMoon } from 'react-icons/fa';
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-center items-center text-gray-1 h-20 px-20">
+      <span className="text-5xl font-bold mr-auto w-32">蟹計</span>
+      <NavLink to="dashboard" Icon={AiFillDashboard} />
+      <NavLink to="graphs" Icon={MdShowChart} />
+      <NavLink to="items" Icon={AiOutlineBars} />
+      <NavLink to="comprehension" Icon={FaBookOpen} />
+      <span className="ml-auto flex justify-end w-32">
+        <FaMoon size={24} />
+      </span>
+    </div>
+  );
+};
+
+const NavLink = ({ to, Icon }) => {
+  const router = useRouter();
+  const active = router.pathname.includes(to);
+
+  return (
+    <Link href={`/${to}`}>
+      <a className="flex justify-center items-center text-lg mx-8">
+        <div className="flex flex-col items-center">
+          <div className="flex my-2.5">
+            <Icon size={24} />
+            <span className="ml-3">
+              {to.charAt(0).toUpperCase() + to.slice(1)}
+            </span>
+          </div>
+          <div
+            className={`h-px w-full bg-gradient-to-r from-bg via-gray-1 to-bg ${
+              !active && 'hidden'
+            }`}
+          ></div>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default Navbar;
---
+++ pages/comprehension.js
@@ -0,0 +1,11 @@
+import Navbar from '@/components/Navbar';
+
+const Comprehension = () => {
+  return (
+    <div className="flex-grow flex flex-col bg-bg">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Comprehension;
